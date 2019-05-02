0304d1a74ddf7b69fe41cb8320741088ee1da05e
jkcrosby3 <42441461+jkcrosby3@users.noreply.github.com>
2019-05-02 06:57:35 -0400

    main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+/*
+*    main.js
+*    Project 2 - Michigan Forests
+*/
+
+var margin = {left:80, right:20, top:50, bottom: 100}
+var height = 500 - margin.top - margin.bottom,
+	width = 800 - margin.left - margin.right;
+						
+var formattedData;
+
+
+	
